5588d9e86b129d2ec2064fa85da31992b7973244
Josh Parry <josh.parry97@gmail.com>
2019-09-16 18:24:50 +1000

    Updates

---
+++ precache-manifest.37077664b2225e74617f01906eb02b14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c975d2d795e88dac19a4b5381dac1ac",
+    "url": "/oxford-hackathon/index.html"
+  },
+  {
+    "revision": "e888404a871ab5b5e519",
+    "url": "/oxford-hackathon/static/css/main.69c5d212.chunk.css"
+  },
+  {
+    "revision": "181e8021393f09ebeb82",
+    "url": "/oxford-hackathon/static/js/2.b5f2d3cd.chunk.js"
+  },
+  {
+    "revision": "e888404a871ab5b5e519",
+    "url": "/oxford-hackathon/static/js/main.070e88c7.chunk.js"
+  },
+  {
+    "revision": "4c15c7f467f44fa9ce8c",
+    "url": "/oxford-hackathon/static/js/runtime~main.c92905d4.js"
+  },
+  {
+    "revision": "0cbd653de0f4f0e9f7c779c947bfef4e",
+    "url": "/oxford-hackathon/static/media/leftLightening.0cbd653d.png"
+  }
+]);
